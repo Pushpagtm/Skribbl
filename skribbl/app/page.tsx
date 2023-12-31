ed475cf8ddf1e1cd0817bdcebd4f9449c0fb7ab6
Pushpagtm <pushpagautam529@gmail.com>
2023-12-31 14:26:27 +0545

    work on drawline in customhook

--- skribbl/app/page.tsx
+++ skribbl/app/page.tsx
@@ -1,14 +1,33 @@
+import { useDraw } from "@/hooks/useDraw";
 import { FC } from "react";
 
 interface pageProps {}
 
 const page: FC<pageProps> = () => {
+  const { canvasRef } = useDraw(drawLine);
+  function drawLine({ currentPoint, prevPoint, ctx }: Draw) {
+    const { x: currX, y: currY } = currentPoint;
+    const lineColor = "#000";
+    const lineWidth = 5;
+    let startPoint = prevPoint ?? currentPoint;
+    ctx.beginPath;
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = lineColor;
+    ctx.moveTo(startPoint.x, startPoint.y);
+    ctx.lineTo(currX, currY);
+    ctx.stroke();
+    ctx.fillStyle = lineColor;
+    ctx.beginPath();
+    ctx.arc(startPoint.x, startPoint.y, 2, 0, 2 * Math.PI);
+    ctx.fill();
+  }
   return (
     <div className="w-screen h-screen bg-white flex justify-center items-center">
       <canvas
         height={550}
         width={550}
         className="border border-black rounded-md"
+        ref={canvasRef}
       />
     </div>
   );
--- skribbl/hooks/useDraw.ts
+++ skribbl/hooks/useDraw.ts
@@ -1,8 +1,25 @@
 import { useEffect, useRef } from "react"
 
-export const useDraw=()=>{
+export const useDraw=(onDraw:({ctx,currentPoint,prevPoint}:Draw)=>void)=>{
     const canvasRef=useRef<HTMLCanvasElement>(null)
     useEffect(()=>{
+        const handler=(e:MouseEvent)=>{
+            const currentPoint=computePointInCanvas(e)
+            const ctx=canvasRef.current?.getContext("2d")
+            if(!ctx||!currentPoint)return
+
+        }
+        const computePointInCanvas=(e:MouseEvent)=>{
+            const canvas=canvasRef.current
+            if(!canvas)return
+            const rect=canvas.getBoundingClientRect()
+            const x=e.clientX-rect.left
+            const y=e.clientY-rect.top
+            return {x,y}
+
+        }
+        canvasRef.current?.addEventListener('mousemove',handler)
+        return ()=>canvasRef.current?.addEventListener('mousemove',handler)
 
     },[])
     return {canvasRef}
