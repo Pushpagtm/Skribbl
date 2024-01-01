b0f6a6a769196e958985f38f928e869c6d2d2e3c
Pushpagtm <pushpagautam529@gmail.com>
2024-01-01 10:12:40 +0545

    some changes

--- skribbl/hooks/useDraw.ts
+++ skribbl/hooks/useDraw.ts
@@ -4,6 +4,7 @@ export const useDraw=(onDraw:({ctx,currentPoint,prevPoint}:Draw)=>void)=>{
     const[mouseDown,setMouseDown]=useState(false)
     const canvasRef=useRef<HTMLCanvasElement>(null)
     const prevPoint=useRef<null | Point>(null)
+    
     const onMouseDown=()=>setMouseDown(true)
     const clearCanvas = () => {
         const canvas = canvasRef.current;
